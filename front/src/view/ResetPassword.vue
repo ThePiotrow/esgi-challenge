1506ae91b7989fa50dfb5b8290ed287e71c59de5
Simon <simonmaltaire@gmail.com>
2023-01-17 18:28:43 +0100

    resetpassword login signup pages OK

--- front/src/view/ResetPassword.vue
+++ front/src/view/ResetPassword.vue
@@ -2,17 +2,11 @@
     <v-row no-gutters justify="center">
         <v-col cols="12" md="6" lg="3">
             <v-card class="pa-3 mt-12">
-                <div class="text-center"><v-icon size="40">mdi-account-circle</v-icon></div>
-                <p class="text-center font-weight-bold">Login</p>
+                <div class="text-center"><v-icon size="40">mdi-lock</v-icon></div>
+                <p class="text-center font-weight-bold">Enter your email address</p>
                 <v-form ref="form" v-model="valid" lazy-validation>
                     <v-text-field v-model="email" :rules="emailRules" label="E-mail" required class="my-4"></v-text-field>
-
-                    <v-text-field v-model="password" label="Password" type="password" required class="mt-4"></v-text-field>
-                    <div class="text-caption mb-4"><router-link to="/resetpassword" class="custom-link">Forgot your password?</router-link></div>
-
-                    <v-btn block color="success" @click="validate"> Login </v-btn>
-                    <v-divider></v-divider>
-                    <div class="text-center my-3"><router-link to="/signup" class="custom-link">Not registered yet?</router-link></div>
+                    <v-btn block color="primary" @click="validate"> Change your password </v-btn>
                 </v-form>
             </v-card>
         </v-col>
@@ -26,8 +20,6 @@ const form = ref();
 const valid = ref(true);
 const email = ref<string>('');
 const emailRules = [(v: string) => !!v || 'E-mail is required', (v: string) => /.+@.+\..+/.test(v) || 'E-mail must be valid'];
-
-const password = ref<string>('');
 
 async function validate() {
     const { valid } = await form.value.validate();
