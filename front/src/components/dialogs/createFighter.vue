e4eab3c866bb57300f4bc6065341d68160f0c1ee
Antoine lin <lin.antoine.pro@gmail.com>
2023-02-10 22:10:29 +0100

    remove pwa, config docker

--- front/src/components/dialogs/createFighter.vue
+++ front/src/components/dialogs/createFighter.vue
@@ -11,7 +11,7 @@
             Register a fighter
           </v-btn>
         </template>
-  
+
         <v-card>
           <v-card-title>
             Register a fighter
@@ -31,17 +31,17 @@
                 </v-row>
                 <v-row class="align-center">
                   <v-col>
-                      <v-autocomplete 
-                          v-model="fighter.nationality" 
-                          :items="nationalityJson" 
-                          solo 
+                      <v-autocomplete
+                          v-model="fighter.nationality"
+                          :items="nationalityJson"
+                          solo
                           :rules="[rules.required]"
-                          variant="outlined" 
-                          label="Nationality" 
+                          variant="outlined"
+                          label="Nationality"
                           placeholder="Nationality"
-                          density="compact" 
-                          color="secondary" 
-                          style="max-height: 450px;" 
+                          density="compact"
+                          color="secondary"
+                          style="max-height: 450px;"
                       />
                   </v-col>
                   <v-col>
@@ -79,126 +79,54 @@
   <script lang="ts">
   import { defineComponent, ref, computed, reactive } from 'vue';
   import nationalityJson from '@/data/nationality.json';
-  import { createToast } from 'mosha-vue-toastify' 
+  import { createToast } from 'mosha-vue-toastify'
   import { FighterI } from '@/interfaces/payload';
-  import { useFighterStore } from '@/stores/fighter'
-  
+  import { useFighterStore } from '@/stores/fighter';
+  import divisionByWeight from '@/utilities/divisionByWeight';
+
   export default defineComponent({
     setup() {
       const fighterStore = useFighterStore();
       const { createFighter } = fighterStore;
-  
+
       const form = ref();
       const dialog = ref<boolean>(false);
       const valid = ref<boolean>(false);
-  
+
       const fighter = reactive<FighterI>({
-          gender: '',
+          gender: null,
           firstname: '',
           lastname: '',
           birthdate: '',
           height: 70,
           weight: 70,
-          nationality: ''
+          nationality: null
       });
-  
-      const divisionByWeight = [
-          {
-              name: 'Strawweight',
-              weight: 52.2,
-              color: '#01682a'
-          },
-          {
-              name: 'Flyweight',
-              weight: 56.7,
-              color: '#1d6a1c'
-          },
-          {
-              name: 'Bantamweight',
-              weight: 61.2,
-              color: '#306c05'
-          },
-          {
-              name: 'Featherweight',
-              weight: 65.8,
-              color: '#416e00'
-          },
-          {
-              name: 'Lightweight',
-              weight: 70.3,
-              color: '#526e00'
-          },
-          {
-              name: 'Super lightweight',
-              weight: 74.8,
-              color: '#50bf8b'
-          },
-          {
-              name: 'Welterweight',
-              weight: 77.1,
-              color: '#636e00'
-          },
-          {
-              name: 'Super welterweight',
-              weight: 79.4,
-              color: '#756d00'
-          },
-          {
-              name: 'Middleweight',
-              weight: 83.9,
-              color: '#886a00'
-          },
-          {
-              name: 'Super middleweight',
-              weight: 88.5,
-              color: '#9b6600'
-          },
-          {
-              name: 'Light heavyweight',
-              weight: 93.0,
-              color: '#af5f00'
-          },
-          {
-              name: 'Cruiserweight',
-              weight: 102.1,
-              color: '#c35600'
-          },
-          {
-              name: 'Heavyweight',
-              weight: 120.2,
-              color: '#d84900'
-          },
-          {
-              name: 'Super heavyweight',
-              weight: 120.3,
-              color: '#ff0000'
-          }
-      ]
-  
+
       const division = computed(() => {
           const closest = divisionByWeight.map(division => division.weight).reduce(function(prev, curr) {
               return (Math.abs(curr - fighter.weight) < Math.abs(prev - fighter.weight) ? curr : prev);
           });
           return divisionByWeight.find(division => division.weight === closest);
       });
-  
+
       const rules = {
           required: (value: any) => !!value || 'Required.',
           weight: (value: number) => (value >= 52 && value <= 400) || 'Weight must be between 52kg and 400kg'
       }
-  
+
       const submitFighter = async () => {
           try {
             const { valid } = await form.value.validate()
-            if (valid) {
-              await createFighter(fighter);
-            }
+              if (valid) {
+                await createFighter(fighter);
+                dialog.value = false;
+              }
           } catch (error: any) {
               createToast(error, { position: "bottom-right", type: 'danger' })
           }
-          dialog.value = false;
       }
-  
+
       return { dialog, valid, fighter, division, nationalityJson, rules, submitFighter, form }
     }
   });
